be31b03694f26fcf7f47c9d6086d4f12e570fac5
Shankar Warang <shankar@edgeware.community>
2026-01-31 19:28:13 +0530

    vercel fix

--- next.config.js
+++ next.config.js
@@ -11,6 +11,14 @@ const nextConfig = {
                 crypto: false,
             };
         }
+
+        // Fix for "Critical dependency: require function is used in a way in which dependencies cannot be statically extracted"
+        // arising from sodium-native being an optional dependency of stellar-sdk
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'sodium-native': false,
+        };
+
         return config;
     },
 }
